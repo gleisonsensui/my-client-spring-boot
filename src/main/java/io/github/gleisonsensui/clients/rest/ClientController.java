bd4c583e36330d2664b8274be31c53f76e1411d5
gleisonsensui <gleison.nsilva@hotmail.com>
2024-10-22 22:31:07 -0300

    created first endpoint api with URI:/api/clients with @RestController and @RequestMapping notation.

---
+++ src/main/java/io/github/gleisonsensui/clients/rest/ClientController.java
@@ -0,0 +1,27 @@
+package io.github.gleisonsensui.clients.rest;
+
+import io.github.gleisonsensui.clients.model.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientController {
+
+    private  final  ClientRepository repository;
+
+    @Autowired
+    public  ClientController(ClientRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client salvar( Client client) {
+        return repository.save(client);
+    }
+}
