c276b4a0547224c09f4e944382a47349b5a2874c
gleisonsensui <gleison.nsilva@hotmail.com>
2024-10-22 23:42:27 -0300

    create endpoint for get user data with repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND))

--- src/main/java/io/github/gleisonsensui/clients/rest/ClientController.java
+++ src/main/java/io/github/gleisonsensui/clients/rest/ClientController.java
@@ -5,6 +5,7 @@ import io.github.gleisonsensui.clients.model.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clients")
@@ -22,5 +23,10 @@ public class ClientController {
     public Client salvar( @RequestBody Client client) {
         return this.repository.save(client);
 
+    }
+
+    @GetMapping("{id}")
+    public Client acharPorId(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
